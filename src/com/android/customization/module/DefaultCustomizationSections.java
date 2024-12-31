4580b196ca9c7601202feec65344c17f0fec37c7
micky387 <mickaelsaibi@free.fr>
2024-12-31 16:39:57 -0500

    ThemePicker: Drop iconpack support
    After the new SystemUi icon feature from aconfig (new_status_bar_icons), this is no longer working, excepted for Settings but useless
    
    Change-Id: I0fcce80f506bb43339acba11403381f50cb327f1
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -11,8 +11,6 @@ import androidx.lifecycle.ViewModelProvider;
 import com.android.customization.model.font.FontManager;
 import com.android.customization.model.font.FontSectionController;
 import com.android.customization.model.grid.GridOptionsManager;
-import com.android.customization.model.iconpack.IconPackManager;
-import com.android.customization.model.iconpack.IconPackSectionController;
 import com.android.customization.model.iconshape.IconShapeManager;
 import com.android.customization.model.iconshape.IconShapeSectionController;
 import com.android.customization.model.theme.OverlayManagerCompat;
@@ -188,11 +186,6 @@ public final class DefaultCustomizationSections implements CustomizationSections
                                         .get(KeyguardQuickAffordancePickerViewModel.class),
                                 lifecycleOwner));
 
-                // Icon pack selection section.
-                sectionControllers.add(new IconPackSectionController(
-                        IconPackManager.getInstance(activity, new OverlayManagerCompat(activity)),
-                        sectionNavigationController));
-
                 // Font selection section.
                 sectionControllers.add(new FontSectionController(
                         FontManager.getInstance(activity, new OverlayManagerCompat(activity)),
@@ -234,11 +227,6 @@ public final class DefaultCustomizationSections implements CustomizationSections
                                 GridOptionsManager.getInstance(activity),
                                 sectionNavigationController,
                                 lifecycleOwner));
-
-                // Icon pack selection section.
-                sectionControllers.add(new IconPackSectionController(
-                        IconPackManager.getInstance(activity, new OverlayManagerCompat(activity)),
-                        sectionNavigationController));
 
                 // Font selection section.
                 sectionControllers.add(new FontSectionController(
